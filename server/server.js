24e9bb90add9d47bafc2f740e63e00b54505824f
thechampp <kaifj99@gmail.com>
2018-07-10 21:03:21 +0530

    add post /users/login route

--- server/server.js
+++ server/server.js
@@ -105,6 +105,17 @@ app.get('/users/me',authenticate,(req,res)=>{
   res.send(req.user);
 });
 
+//user login
+app.post('/users/login',(req,res)=>{
+  var body = _.pick(req.body,['email','password']);
+
+  User.findByCredentials(body.email,body.password).then((user)=>{
+    return user.generateAuthToekn().then((token)=>{
+      res.header('x-auth',token).send(user);
+    });
+  }).catch((e)=>res.status(400).send(e));
+});
+
 app.listen(port,()=>{
   console.log('Server running on port '+port);
 });
